9c28cdb3bf410aee75ac06ff53ca621f1561883e
froleyks <nfroleyks@gmail.com>
2024-06-04 16:06:52 +0200

    added readme

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -1,26 +1,24 @@
 #!/usr/bin/env bash
 set -e
-dir=$(cd "$(dirname "$0")" && pwd)
-cd ..
+dir=$(cd "$(dirname "$0")/.." && pwd)
+cd "$dir"
 git clone https://github.com/arminbiere/kissat.git
 git clone https://github.com/arminbiere/aiger.git
-(cd kissat && ./configure && make -j4)
-cd aiger
+cd "$dir"/kissat
+./configure && make -j8
+cd "$dir"/aiger
 sed -i 's/assert (reset <= 1 || reset == lit);//' aiger.c
-./configure.sh && make
+./configure.sh && make -j8
 echo Suceyssfully build kissat and aiger library
 if [ -n "$1" ]; then
-    echo "Downloading qbf dependecies for non-stratified resets"
-    cd ..
-    git clone https://github.com/ltentrup/quabs.git
-    cd quabs
-    git submodule init
-    git submodule update
-    mkdir build
-    cd build
+    echo "Downloading QAIGER dependencies to deal with non-stratified resets"
+    cd "$dir"
+    git clone --recurse-submodules https://github.com/ltentrup/quabs.git
+    cd "$dir"/quabs
+    mkdir build && cd build
     cmake -DCMAKE_BUILD_TYPE=Release ..
-    make -j4
+    make
 fi
-cd $dir
-./configure && make -j4
+cd "$dir"/certifaiger
+./configure && make -j8
 ./check_certificate.sh examples/model.aag examples/witness.aag
